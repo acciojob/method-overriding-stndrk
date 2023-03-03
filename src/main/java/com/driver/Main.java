55bec81e76e5297956897a5f118a092c93ccd41b
satendra kumar <satendra.km@alumni.iitd.ac.in>
2023-03-03 19:52:53 +0530

    method overriding 2nd

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,14 +2,10 @@ package com.driver;
 
 public class Main{
     public static class A{
-        public String meth(){
-            return "Invoking method from class A";
-            }
+        public String meth(){return "Invoking method from class A";}
     }
     public static class B extends A{
-        public String meth(){
-            return "Invoking method from class B";
-            }
+        public String meth(){return "Invoking method from class B";}
     }
     public static void main(String[] args) {
 		B temp=new B();
