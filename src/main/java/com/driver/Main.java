9b23e6a1abd5073e6bd0c8e2472f2553341fb10c
satendra kumar <satendra.km@alumni.iitd.ac.in>
2023-03-03 18:07:45 +0530

    Method overriding

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,20 @@
 package com.driver;
-
-public class Main {
-  
+class A{
+    String meth(){
+        return "Invoking method from class A";
+    }
+ }
+ class B extends A{
+    String meth(){
+        return "Invoking method from class B";
+    }
+ }
+ 
+public class Main
+{
+	public static void main(String[] args) {
+		B temp=new B();
+        String res=temp.meth();
+        System.out.print(res);
+	}
 }
